bdaf36ba0dbe68a1f9564192cd36cf53a85c1e2a
pavliukkk <126521423+pavliukkk@users.noreply.github.com>
2023-12-18 17:46:07 +0200

    Add files via upload

--- js/register_correct.js
+++ js/register_correct.js
@@ -4,43 +4,61 @@ document.addEventListener("DOMContentLoaded", function() {
     const differentPasswordsError = document.getElementById('different_passwords');
     const shortPasswordsError = document.getElementById('short_passwords');
     const weakPasswordError = document.getElementById('weak_password');
+    const fillError = document.getElementById('fill_error');
     const signUpForm = document.querySelector('.register_form');
+    const phoneNumberField = document.querySelector('input[name="phone_number"]');
 
     differentPasswordsError.style.display = 'none';
     shortPasswordsError.style.display = 'none';
     weakPasswordError.style.display = 'none';
+    fillError.style.display = 'none';
+
+    phoneNumberField.addEventListener('input', function() {
+        // Обмежуємо введення лише цифр та до 10 символів
+        const regex = /[^\d]/g;
+        phoneNumberField.value = phoneNumberField.value.replace(regex, '').substring(0, 10);
+    });
 
     signUpForm.addEventListener('submit', function(event) {
         event.preventDefault(); // Зупиняємо стандартну відправку форми
 
         const password = passwordField.value;
         const confirmPassword = confirmPasswordField.value;
+        const surname = document.querySelector('input[name="surname"]').value;
+        const name = document.querySelector('input[name="name"]').value;
+        const email = document.querySelector('input[name="email"]').value;
+        const phoneNumber = phoneNumberField.value;
 
-        if (password !== confirmPassword) {
-            differentPasswordsError.style.display = 'block';
+        if (!surname || !name || !email || !phoneNumber || !password || !confirmPassword) {
+            fillError.style.display = 'block';
         } else {
-            differentPasswordsError.style.display = 'none';
-        }
+            fillError.style.display = 'none';
 
-        if (password.length < 8) {
-            shortPasswordsError.style.display = 'block';
-        } else {
-            shortPasswordsError.style.display = 'none';
-        }
+            if (password !== confirmPassword) {
+                differentPasswordsError.style.display = 'block';
+            } else {
+                differentPasswordsError.style.display = 'none';
+            }
 
-        const containsLetter = /[a-zA-Z]/.test(password); // Перевірка наявності букви в паролі
+            if (password.length < 8) {
+                shortPasswordsError.style.display = 'block';
+            } else {
+                shortPasswordsError.style.display = 'none';
+            }
 
-        if (!containsLetter) {
-            weakPasswordError.style.display = 'block';
-        } else {
-            weakPasswordError.style.display = 'none';
-        }
+            const containsLetter = /[a-zA-Z]/.test(password);
 
-        // Якщо умови виконані, то можна відправити форму
-        if (differentPasswordsError.style.display === 'none' && 
-            shortPasswordsError.style.display === 'none' &&
-            weakPasswordError.style.display === 'none') {
-            this.submit();
+            if (!containsLetter) {
+                weakPasswordError.style.display = 'block';
+            } else {
+                weakPasswordError.style.display = 'none';
+            }
+
+            if (differentPasswordsError.style.display === 'none' &&
+                shortPasswordsError.style.display === 'none' &&
+                weakPasswordError.style.display === 'none') {
+                this.submit();
+            }
         }
     });
 });
